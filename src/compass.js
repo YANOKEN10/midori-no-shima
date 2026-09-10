3b11c258b818a5d7c563a89387464883dc6317be
voraz <voraz.yanokenta-arcoiris0928@docomo.ne.jp>
2026-09-10 20:19:38 +0900

    Set individual follower sizes for all 153 Gaon

--- src/compass.js
+++ src/compass.js
@@ -1,7 +1,7 @@
 // ============================================================
 //  リーフ・コンパス（つぎの ものがたりの もくてきち）
 // ============================================================
-import { MAPS } from "./data/maps.js?v=20260910-landscape-v20";
+import { MAPS } from "./data/maps.js?v=20260910-follower-size-v21";
 import { G as State, flag, hasItem } from "./state.js";
 
 const EMBLEM_TARGETS = [
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,4 @@
-import { heroFrame } from "./revampArt.js?v=20260910-landscape-v20";
+import { heroFrame } from "./revampArt.js?v=20260910-follower-size-v21";
 import { battleArt } from './data/battleart.js';
 import { chapterObjective } from "./chapterStory.js";
 // ============================================================
